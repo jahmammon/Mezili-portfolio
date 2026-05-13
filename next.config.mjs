0d4f6c6f2e175718df87a1afb2aa38fb9527e432
jahmammon <122760477+jahmammon@users.noreply.github.com>
2026-05-13 19:41:07 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
-    experimental: {
-        optimizePackageImports: ["lucide-react"],
-          },
-          
-            images: {
-                formats: ["image/avif", "image/webp"],
-                    remotePatterns: [],
-                      },
-                      };
-                      
-                      export default nextConfig; */
+};
+
+export default nextConfig;
